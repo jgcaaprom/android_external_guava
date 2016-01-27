TeamExodus/external_guava

Fix compilation with jdk8
Change-Id: Ifa2f67f0b1a42daf70bf125784ad4c1b5eeb3ddc

guava/src/com/google/common/base/Splitter.java
@@ -372,7 +372,7 @@ public final class Splitter {
 
     return new Iterable<String>() {
       @Override public Iterator<String> iterator() {
-        return spliterator(sequence);
+        return Splitter.this.spliterator(sequence);
       }
     };
   }

jgcaaprom/android_external_guava

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
Conflicts:
	guava/src/com/google/common/base/Splitter.java

guava/src/com/google/common/base/Splitter.java
@@ -387,8 +387,9 @@ public final class Splitter {
 
     return new Iterable<String>() {
       @Override public Iterator<String> iterator() {
-        return splittingIterator(sequence);
+        return Splitter.this.splittingIterator(sequence);
       }
+      
       @Override public String toString() {
         return Joiner.on(", ")
             .appendTo(new StringBuilder().append('['), this)